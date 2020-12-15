fe9a8f68138e8e8440d35fcdd7d5bb01916435e1
Javier Carrasco <javier.carrasco@edorasware.com>
2020-12-15 11:42:06 +0100

    Mobile share button

--- src/CalculadoraMobile.svelte
+++ src/CalculadoraMobile.svelte
@@ -30,6 +30,21 @@
   );
 
   let windowInfo = false;
+
+  async function share() {
+    if (!navigator.share) return;
+    const shareData = {
+      title: document.title,
+      text: `Volumen: ${ww} * ${ll} * ${hh} m³, ventilacion: ${vLabels[vent]} (${vRen[vent]} ACH). CADR necesario: ${needCADR}`,
+      url: window.location.href
+    };
+    try {
+      await navigator.share(shareData);
+      gtag("event", "share", { ...shareData, success: true });
+    } catch (err) {
+      gtag("event", "share", { ...shareData, success: false });
+    }
+  }
 </script>
 
 <div class="w-full">
@@ -56,14 +71,40 @@
     <Room {l} {w} {h} {vent} />
   </div>
 
-  <div class="mx-2">
+  <div class="flex mx-2">
     <Meters bind:value={hh} label="Alto" />
+    {#if navigator.share}
+      <div class="w-1/2 flex items-end justify-end">
+        <button
+          title="Compartir"
+          class="rounded-full bg-purple-700 shadow text-white w-10 h-10 flex
+          items-center justify-center">
+          <svg
+            class="w-6 h-6 -ml-1"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M8.684 13.342C8.886 12.938 9 12.482 9
+              12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0
+              2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3
+              0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0
+              00-5.368-2.684z" />
+          </svg>
+        </button>
+      </div>
+    {/if}
   </div>
 
   <div class="ml-3 pt-3">
     Ventilación existente {vRen[vent]} ACH
     <button
       type="button"
+      title="Información sobre la ventilación"
       class=" rounded-full bg-blue-500 text-white font-bold w-6 h-6 ml-3 shadow
       hover:bg-blue-400"
       on:click={() => (windowInfo = true)}>
